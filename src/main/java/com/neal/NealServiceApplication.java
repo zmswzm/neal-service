ac3bd2f0deeaf75722cfe121c5b76e9fdcb9f886
zmswzm <suwei_neal@icloud.com>
2018-12-13 01:47:05 +0800

    delete gson

--- src/main/java/com/neal/NealServiceApplication.java
+++ src/main/java/com/neal/NealServiceApplication.java
@@ -1,11 +1,10 @@
-package com.neal.nealservice;
+package com.neal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { JacksonAutoConfiguration.class, DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class NealServiceApplication {
 
     public static void main(String[] args) {
